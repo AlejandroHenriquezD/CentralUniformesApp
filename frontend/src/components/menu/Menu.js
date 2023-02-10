69438c764bd64e709a58005da8f13815b8fb9816
davidcasimiro77 <casimiroherreradavid2@gmail.com>
2023-02-10 18:42:02 +0000

    HelpNDoc

--- frontend/src/components/menu/Menu.js
+++ frontend/src/components/menu/Menu.js
@@ -11,7 +11,7 @@ const Menu = () => {
       <a href="/show_articulos">Artículos</a>
       <a href="/show_trabajos">Trabajos</a>
       <a href="http://localhost:5488/templates/rkJTnK2ce">Informe</a>
-      <a href="http://localhost:8080/Ayuda.html">Help</a>
+      <a href="http://localhost/dashboard/hnd/ayuda.html">Help</a>
     </div>
   );
 };
